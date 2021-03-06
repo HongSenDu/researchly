66b81a763539f0f218aa8c01f52659c257b1314b
mwinitch <michaelwin26@gmail.com>
2021-03-06 12:51:56 -0500

    Basic index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
   resources :groups
   resources :deliverables
   resources :projects
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'welcome#index'
 end
