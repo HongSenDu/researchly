27acee5f9d65db400689a381e3a94aff4f2312d9
mwinitch <michaelwin26@gmail.com>
2021-03-06 12:38:06 -0500

    Get rid of old migrations

--- db/migrate/20210305210555_drop_groups_users.rb
+++
@@ -1,5 +0,0 @@
-class DropGroupsUsers < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :groups_users
-  end
-end
--- db/migrate/20210305212845_drop_membership.rb
+++
@@ -1,5 +0,0 @@
-class DropMembership < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :membership
-  end
-end
