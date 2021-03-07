7aba5f8f1a5a6044e1feb84081770d31470d7fc6
HongSenDu <hongsendu@gmail.com>
2021-03-07 14:45:50 -0500

    added seeds.rb file for database entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = [{:email => "michael@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'},
+    {:email => "aaron@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'},
+    {:email => "andrew@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'},
+    {:email => "hong@gmail.com", :password => 'topsecret', :password_confirmation => 'topsecret'},
+  ]
+
+users.each do |user|
+    User.create!(user)
+end
+
+
+groups = [{:name => "Group 1", :description => "cool group"},
+          {:name => "Group 2", :description => "not cool group"},
+          {:name => "Group 3", :description => "decent group"},
+          {:name => "Group 4", :description => "test group"},
+          {:name => "Group 5", :description => "group 5"},
+          {:name => "Group 6", :description => "group 6"}
+        ]
+
+groups.each do |group|
+    Group.create!(group)
+end
+
+
+
+projects = [{:name => "Project 1", :description => "", :group_id => 1},
+    {:name => "Project 2", :description => "", :group_id => 2},
+    {:name => "Project 3", :description => "", :group_id => 3},
+    {:name => "Project 4", :description => "", :group_id => 4}
+  ]
+
+projects.each do |project|
+    Project.create!(project)
+end
+
+deliverables = [{:name => "deliverable 1", :description => "", :status => "done", :project_id => 1},
+    {:name => "deliverable 2", :description => "", :status => "ongoing", :project_id => 1},
+    {:name => "deliverable 3", :description => "", :status => "done", :project_id => 2},
+    {:name => "deliverable 4", :description => "", :status => "ongoing", :project_id => 3},
+    {:name => "deliverable 5", :description => "", :status => "ongoing", :project_id => 3},
+    {:name => "deliverable 6", :description => "", :status => "done", :project_id => 3}
+  ]
+
+deliverables.each do |deliverable|
+    Deliverable.create!(deliverable)
+end
+
+memberships = [{:user_id => 1, :group_id => 1},
+    {:user_id => 1, :group_id => 2},
+    {:user_id => 2, :group_id => 1},
+    {:user_id => 3, :group_id => 3},
+    {:user_id => 3, :group_id => 2},
+    {:user_id => 4, :group_id => 1},
+    {:user_id => 3, :group_id => 3}
+  ]
+
+memberships.each do |membership|
+    Membership.create!(membership)
+end
+
