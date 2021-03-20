076d9e2dd4254caa076f787187e0d129de54b6bc
HongSenDu <hongsendu@gmail.com>
2021-03-19 20:30:22 -0400

    added deadline to deliverable

--- db/migrate/20210319235716_add_status_to_deliverable.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToDeliverable < ActiveRecord::Migration[6.1]
-  def change
-    add_column :deliverables, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_235652) do
+ActiveRecord::Schema.define(version: 2021_03_20_002937) do
 
   create_table "deliverables", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_235652) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "project_id", null: false
+    t.datetime "deadline"
     t.index ["project_id"], name: "index_deliverables_on_project_id"
   end
 
