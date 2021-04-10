fb7f9d683522cc352642c8696118297775ae9868
54aaron <arj2145@columbia.edu>
2021-04-09 23:14:19 -0400

    added back button to activities show page. fixed rspec

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "Activities", type: :request do
+RSpec.describe ActivitiesController, type: :controller do
 
   context "GET /show" do
     it "returns http success" do
-      get "/activities/show"
+        get(:show, params: {'id' => "1"}, session: {'group_id' => 1})
       expect(response).to have_http_status(:success)
     end
     it 'renders activities show view' do
-      get "/activities/show"
+        get(:show, params: {'id' => "1"}, session: {'group_id' => 1})
       expect(response).to render_template('show')
     end
   end
