11fa0efec22d4d0d36bef8b6e04291ae2b645393
54aaron <arj2145@columbia.edu>
2021-04-20 21:07:26 -0400

    domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "researchly_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'researchly-app-4.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'researchly-app.herokuapp.com' }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
